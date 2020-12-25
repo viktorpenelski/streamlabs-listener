2ff2d0a9d127c8a55c7d8e06240bfdd9e4bed9b1
vpenelski <victor.penelski@gmail.com>
2020-12-25 22:27:20 +0200

    arduino integration

--- streamlabs-listener/src/main/kotlin/com/github/viktorpenelski/App.kt
+++ streamlabs-listener/src/main/kotlin/com/github/viktorpenelski/App.kt
@@ -1,5 +1,7 @@
 package com.github.viktorpenelski
 
+import com.github.viktorpenelski.arduino.Arduino
+import com.github.viktorpenelski.arduino.RGBAnimations
 import com.github.viktorpenelski.repo.DonationRepositoryJdbc
 import com.github.viktorpenelski.repo.JdbcConfig
 import com.github.viktorpenelski.streamlabs.DonationMessage
@@ -7,13 +9,10 @@ import com.github.viktorpenelski.streamlabs.StreamlabsClient
 import com.github.viktorpenelski.streamlabs.StreamlabsConfig
 import io.github.cdimascio.dotenv.Dotenv
 import io.github.cdimascio.dotenv.dotenv
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.BroadcastChannel
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.consumeEach
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import java.util.*
 
 @ExperimentalCoroutinesApi
@@ -22,10 +21,20 @@ fun main() = runBlocking {
     val client = StreamlabsClient(channel, StreamlabsConfig())
     val dotenv = initializeDotEnv()
     val donationRepository = DonationRepositoryJdbc(initializeJdbcConfig(dotenv))
+    val arduino = Arduino()
 
     client.connect()
-    sendDonationsToMapper(channel.openSubscription(), donationRepository, ::processDonation)
-    logDonations(channel.openSubscription())
+    launch {
+        logDonations(channel.openSubscription())
+    }
+    withContext(Dispatchers.Default) {
+        launch {
+            sendDonationsToMapper(channel.openSubscription(), donationRepository, ::processDonation)
+        }
+        launch {
+            controlRGB(arduino, channel.openSubscription())
+        }
+    }
 
     println()
 }
@@ -59,6 +68,20 @@ suspend fun logDonations(channel: ReceiveChannel<DonationMessage>) = coroutineSc
     launch {
         channel.consumeEach {
             println(it)
+        }
+    }
+}
+
+suspend fun controlRGB(arduino: Arduino, channel: ReceiveChannel<DonationMessage>) = coroutineScope {
+    launch {
+        channel.consumeEach {
+            println("attempting to send")
+            arduino.send(RGBAnimations.THEATER_RAINBOW_CHASE)
+            launch {
+                delay(2000L)
+                println("after delay")
+                arduino.send(RGBAnimations.RAINBOW_CYCLE)
+            }
         }
     }
 }
